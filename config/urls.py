6381946fe9b2faab0bc5797c4382ac457ed34506
Fernando Macombo <92873560+fernandomacombo@users.noreply.github.com>
2026-08-01 19:37:55 +0200

    adiciona rotas base da API

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,16 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # Páginas Django atuais
     path("", include("entradas.urls")),
+
+    # API preparada para o futuro frontend React + Tailwind
+    path("api/", include("entradas.api_urls")),
 ]
 
 if settings.DEBUG:
